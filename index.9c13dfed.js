8130f6fb6ca8f2067127feeb98502c4afd2a7f67
an1skat <156462349+an1skat@users.noreply.github.com>
2024-05-16 17:22:16 +0000

    Deploying to gh-pages from @ an1skat/webovio@24c74e4bdb8fe8a2cb32fee9f47025e3aaac8d22 🚀

---
+++ index.9c13dfed.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".scroll-to-top-btn");window.addEventListener("scroll",(function(){window.scrollY>100?o.style.display="flex":o.style.display="none"}));o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}))}();
+//# sourceMappingURL=index.9c13dfed.js.map

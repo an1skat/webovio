ec3bb8dfb8f462c3f50b0da4ba661ca78786d899
an1skat <156462349+an1skat@users.noreply.github.com>
2024-05-18 11:15:02 +0000

    Deploying to gh-pages from @ an1skat/webovio@0d723534598032cdbcc9a926fe78e2bdec9ab664 🚀

--- index.27b12ca8.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector(".scroll-to-top-btn");window.addEventListener("scroll",(()=>{window.scrollY>100?o.style.display="flex":o.style.display="none"}));o.addEventListener("click",(()=>{window.scrollTo({top:0,behavior:"smooth"})}));
-//# sourceMappingURL=index.27b12ca8.js.map
